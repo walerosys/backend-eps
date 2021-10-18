4f29a023d70cfc672e6fde47e24d991925bb0325
walerosys <wagnerleonramos@gmail.com>
2021-10-18 13:10:27 -0500

    correccion consulta

--- app/Http/Controllers/InstalacionController.php
+++ app/Http/Controllers/InstalacionController.php
@@ -47,28 +47,31 @@ public function registerinsta(Request $request){
                 $detalle->save();
             }
                         /****************join start******************/
-                       /* $datos= DB::table('instalacions')
-                        ->join('tipodeinstalacions', 'instalacions.tipo_id', '=', 'tipodeinstalacions.id')
+                        $datos= DB::table('tipodeinstalacions')
+                        ->join('instalacions', 'tipodeinstalacions.id', '=', 'instalacions.tipo_id')
                         ->join('usuario_del_servicios', 'instalacions.uservicio_id', '=', 'usuario_del_servicios.id')
-                        ->join('users', 'instalacions.user_id', '=', 'users.id')
-                        ->join('instalacions_actividads', 'instalacions.id', '=', 'instalacions_actividads.instalacion_id')
-                        ->join('actividads', 'instalacions_actividads.actividad_id', '=', 'actividads.id')
                         ->where('instalacions.id', '=',$instal->id)
                         ->select(
-                           'tipodeinstalacions.nombre',
+                           'codigo',
+                           'tipodeinstalacions.nombre AS nombre_tipo',
                            'instalacions.*',
-                           'usuario_del_servicios.*',
-                           'users.*',
-                           'instalacions_actividads.*',
-                           'actividads.*')
-                        ->get();*/
+                           'usuario_del_servicios.nombre',
+                           'apellidos',
+                           'tipo',
+                           'razon_social',
+                           'ruc',
+                           'dni',
+                           'direccion',
+                           'codigo_catastral',
+                           'inscripcion')
+                        ->get();
                         /****************join end******************/
 
             return response()->json([
                 "status" => 200,
                 "success" => true,
                 "message" => "Registro completado con éxito",
-                "data" => "ssfd"
+                "data" => $datos
             ]);
     }
 }
